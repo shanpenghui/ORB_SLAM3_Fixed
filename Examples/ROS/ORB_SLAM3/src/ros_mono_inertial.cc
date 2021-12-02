1216451889af3c58251edc9df69cd9ad30569b55
liuyh <liuyh47@midea.com>
2021-12-02 19:47:46 +0800

    add vio odom publish

--- Examples/ROS/ORB_SLAM3/src/ros_mono_inertial.cc
+++ Examples/ROS/ORB_SLAM3/src/ros_mono_inertial.cc
@@ -34,7 +34,18 @@
 #include"../../../include/System.h"
 #include"../include/ImuTypes.h"
 
+#include<Eigen/Dense>
+//新增发布Odometry
+#include<nav_msgs/Odometry.h>
+
 using namespace std;
+using namespace ros;
+
+ros::Publisher pub_odometry;
+
+//定义第一帧flag，用于时间戳作差
+bool first_image_flag = true;
+double first_image_time;
 
 class ImuGrabber
 {
@@ -73,6 +84,10 @@ int main(int argc, char **argv)
   ros::NodeHandle n("~");
   ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Info);
   bool bEqual = false;
+
+  //发布odometry话题
+  pub_odometry = n.advertise<nav_msgs::Odometry>("odometry",1000);
+
   if(argc < 3 || argc > 4)
   {
     cerr << endl << "Usage: rosrun ORB_SLAM3 Mono_Inertial path_to_vocabulary path_to_settings [do_equalize]" << endl;
@@ -95,7 +110,7 @@ int main(int argc, char **argv)
   ImageGrabber igb(&SLAM,&imugb,bEqual); // TODO
   
   // Maximum delay, 5 seconds
-  ros::Subscriber sub_imu = n.subscribe("/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
+  ros::Subscriber sub_imu = n.subscribe("/camera/imu", 1000, &ImuGrabber::GrabImu, &imugb); 
   ros::Subscriber sub_img0 = n.subscribe("/camera/image_raw", 100, &ImageGrabber::GrabImage,&igb);
 
   std::thread sync_thread(&ImageGrabber::SyncWithImu,&igb);
@@ -112,6 +127,19 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr &img_msg)
     img0Buf.pop();
   img0Buf.push(img_msg);
   mBufMutex.unlock();
+
+  //判断是否第一帧
+  if(first_image_flag)
+  {
+    first_image_flag = false;
+    first_image_time = img_msg->header.stamp.toSec();
+    return;
+  }
+  ofstream f("/home/user/Documents/stamp.txt", ios::app);
+  f.setf(ios::fixed, ios::floatfield);
+  f.precision(10);
+  f << img_msg->header.stamp.toSec() - first_image_time << endl;
+  f.close();
 }
 
 cv::Mat ImageGrabber::GetImage(const sensor_msgs::ImageConstPtr &img_msg)
@@ -138,6 +166,26 @@ cv::Mat ImageGrabber::GetImage(const sensor_msgs::ImageConstPtr &img_msg)
   }
 }
 
+
+//新增pubOdometry用于发布里程计数据
+void pubOdometry(const vector<float> &t, const vector<float> &q, const std_msgs::Header &header)
+{
+  nav_msgs::Odometry odometry;
+  odometry.header = header;
+  odometry.header.frame_id = "vio";
+  //odometry.child_fram_id = "base_link";
+  odometry.pose.pose.position.x = t[0];
+  odometry.pose.pose.position.y = t[1];
+  odometry.pose.pose.position.z = t[2];
+  odometry.pose.pose.orientation.x = q[0];
+  odometry.pose.pose.orientation.y = q[1];
+  odometry.pose.pose.orientation.z = q[2];
+  odometry.pose.pose.orientation.w = q[3];
+
+  pub_odometry.publish(odometry);
+
+}
+
 void ImageGrabber::SyncWithImu()
 {
   while(1)
@@ -146,6 +194,7 @@ void ImageGrabber::SyncWithImu()
     double tIm = 0;
     if (!img0Buf.empty()&&!mpImuGb->imuBuf.empty())
     {
+      std_msgs::Header header = img0Buf.front()->header;
       tIm = img0Buf.front()->header.stamp.toSec();
       if(tIm>mpImuGb->imuBuf.back()->header.stamp.toSec())
           continue;
@@ -175,7 +224,45 @@ void ImageGrabber::SyncWithImu()
       if(mbClahe)
         mClahe->apply(im,im);
 
-      mpSLAM->TrackMonocular(im,tIm,vImuMeas);
+      // mpSLAM->TrackMonocular(im,tIm,vImuMeas);
+
+      //获得cam在word系下的表达 tcw
+      cv::Mat tmp_tcw = cv::Mat::eye(4,4,CV_32F);
+      tmp_tcw = mpSLAM->TrackMonocular(im,tIm,vImuMeas);
+
+      //当tracking faild时tcw为空
+      if(tmp_tcw.empty()) 
+      {
+        cout << "Tcw empty" << endl;
+      }
+      else
+      {
+        //将 tcw 中的旋转部分拆开，得到rcw
+        cv::Mat tmp_rcw = cv::Mat::eye(3,3,CV_32F); 
+        tmp_rcw = tmp_tcw.rowRange(0,3).colRange(0,3).t();
+        //将rcw矩阵形式转换成四元数
+        Eigen::Matrix<double,3,3> M;
+        M << tmp_rcw.at<float>(0,0), tmp_rcw.at<float>(0,1), tmp_rcw.at<float>(0,2),
+            tmp_rcw.at<float>(1,0), tmp_rcw.at<float>(1,1), tmp_rcw.at<float>(1,2),
+            tmp_rcw.at<float>(2,0), tmp_rcw.at<float>(2,1), tmp_rcw.at<float>(2,2);
+
+        Eigen::Quaterniond q(M);
+
+        std::vector<float> v(4);
+        v[0] = q.x();
+        v[1] = q.y();
+        v[2] = q.z();
+        v[3] = q.w();
+        
+        //将 tcw 中的平移部分拆开，得到t
+        std::vector<float > t(3);
+        t[0] = tmp_tcw.at<float>(0,3);
+        t[1] = tmp_tcw.at<float>(1,3);
+        t[2] = tmp_tcw.at<float>(2,3);
+
+        pubOdometry(t, v, header);
+      }
+
     }
 
     std::chrono::milliseconds tSleep(1);
